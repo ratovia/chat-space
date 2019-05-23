75e61c9ff96fd23ce4335dd92d9f88f4168642e9
ratovia <ratovia@hotmail.co.jp>
2019-05-23 16:35:33 +0900

    generate groups and group-users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "group#index"
-  resources :users, only: [:edit, :update]
+  root "users#show"
+  resources :users, only: [:show, :edit, :update]
 end
