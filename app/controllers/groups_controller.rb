7c23c2ff56e8efd01a403e6fb25710ad3c337c5f
ratovia <ratovia@hotmail.co.jp>
2019-05-24 17:14:12 +0900

    append group function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,38 @@
 class GroupsController < ApplicationController
+  protect_from_forgery except: :create
+  before_action :set_group, except: [:new,:create]
+
+  def new
+    @group = Group.new
+    @group.users << current_user
+  end
+
+  def create
+    @group = Group.new(groups_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @group.update(groups_params)
+      redirect_to group_messages_path(@group)
+    else
+      render :edit
+    end
+  end
+
+  private
+    def groups_params
+      params.require(:group).permit(:name, { user_ids: [] })
+    end
+
+    def set_group
+      @group = Group.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "users#show"
   resources :users, only: [:show, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index,:create]
+  end
 end
