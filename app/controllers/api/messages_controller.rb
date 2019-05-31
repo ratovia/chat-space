04e4bf52fc73204d23418fef00b3cf23b9221077
ratovia <ratovia@hotmail.co.jp>
2019-05-31 15:39:43 +0900

    delete message_params method

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,13 +1,8 @@
 class Api::MessagesController < ApplicationController
   def index
-    @messages = Message.where("id > ?",message_params[:message_id])
+    @messages = Message.where("id > ?",params[:message_id])
     respond_to do |format|
       format.json
     end
   end
-
-  private
-    def message_params
-      params.permit(:message_id)
-    end
 end
