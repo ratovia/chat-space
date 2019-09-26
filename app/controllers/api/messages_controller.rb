99e5cc36eb2c6ed88ac0bc0c48f4fb6f3e09cde0
ratovia <ratovia@hotmail.co.jp>
2019-09-26 17:50:03 +0900

    update chat-space

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::MessagesController < ApplicationController
   def index
-    @messages = Message.where("id > ?",params[:message_id])
+    @messages = Message.where("id > ?",params[:message_id]).where("group_id = ?",params[:group_id])
     respond_to do |format|
       format.json
     end
